de447cefdd1b7d8110bfc042c94a54fac73e674e
bielgeri <geribolarodrigues@gmail.com>
2026-09-25 15:29:50 -0300

    Repository Config

---
+++ src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -0,0 +1,12 @@
+package com.devsuperior.movieflix.entities;
+
+public class Movie {
+
+	private Long id;
+	private String title;
+	private String subTitle;
+	private Integer year;
+	private String imgUrl;
+	private String synopsis;
+	
+}
---
+++ src/main/java/com/devsuperior/movieflix/entities/Review.java
@@ -0,0 +1,63 @@
+package com.devsuperior.movieflix.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_review")
+public class Review {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String text;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	public Review() {}
+	
+	public Review(Long id, String text) {
+		super();
+		this.id = id;
+		this.text = text;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Review other = (Review) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
